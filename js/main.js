53112a770b9c5dba5a88ca2960e0c0294d8809b4
JuliaKuvsh <juliakachergina@mail.ru>
2022-06-05 17:55:40 +0300

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Функция возвращает случайное целое число из переданного диапазона включительно
+
+function getRandomNumber (min, max) {
+  if (min >= 0 && max >= 0 && max > min) {
+    return Math.floor(Math.random() * (max - min) + min);
+  } else if (min >= 0 && max >= 0 && min === max) {
+    return min;
+  }
+  throw new Error ('Диапазон указан неверно. Числа должны быть больше или равны нулю, а первое число - меньше второго.');
+}
+
+getRandomNumber (10, 15);
+
+//Функция проверки максимальной длины строки
+
+function checkLength (string, maxLength) {
+  if (string.length <= maxLength) {
+    return true;
+  }
+  return false;
+}
+
+checkLength ('Пример строки', 15);
